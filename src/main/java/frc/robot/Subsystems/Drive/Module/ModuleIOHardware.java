88fc6fc716456e1d1845b6383d38ff0a0fe3cbec
RuhanRajasthan <144182793+RuhanRajasthan@users.noreply.github.com>
2025-11-17 15:23:43 -0700

    basic work on constants in ModuleIOHardware
    not gonna build cuz school computer

--- src/main/java/frc/robot/Subsystems/Drive/Module/ModuleIOHardware.java
+++ src/main/java/frc/robot/Subsystems/Drive/Module/ModuleIOHardware.java
@@ -20,11 +20,46 @@ public class ModuleIOHardware implements ModuleIO{
     private final Queue<Double> drivePosQueue;
     private final Queue<Double> turnPosQueue;
 
+    private double absoluteEncoderOffset;
+    private final int driveMotorID;
+    private final int turnMotorID;
+
     private final Debouncer driveDebouncer = new Debouncer(.5);
     private final Debouncer turnDebouncer = new Debouncer (.5);
 
 
     public ModuleIOHardware (int moduleID){
+        absoluteEncoderOffset =
+            switch(moduleID){
+                case 0 -> Constants.DriveConstants.frontLeftOffset;
+                case 1 -> Constants.DriveConstants.frontRightOffset;
+                case 2 -> Constants.DriveConstants.backLeftOffset;
+                case 3 -> Constants.DriveConstants.backRightOffset;
+                default -> new Rotation2d();
+            };
+
+        driveMotorID =
+            switch(moduleID){
+                case 0 -> Constants.DriveConstants.frontLeftDriveCanId;
+                case 1 -> Constants.DriveConstants.frontRightDriveCanId;
+                case 2 -> Constants.DriveConstants.backLeftDriveCanId;
+                case 3 -> Constants.DriveConstants.backRightDriveCanId;
+                default -> -999999
+            };
         
+        turnMotorID =
+            switch (moduleID) {
+                case 0 -> Constants.DriveConstants.frontLeftTurnCanId;
+                case 1 -> Constants.DriveConstants.frontRightTurnCanId;
+                case 2 -> Constants.DriveConstants.backLeftTurnCanId;
+                case 3 -> Constants.DriveConstants.backRightTurnCanId;
+                default -> -999999;
+            };
+
+        
+
+        
+        
+
     }
 }
