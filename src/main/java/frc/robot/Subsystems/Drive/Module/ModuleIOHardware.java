95f6118a24f1c6d2f63109057eebe994ec6f09d4
RuhanRajasthan <rsharma@cherrycreekschools.org>
2025-11-19 22:02:17 -0700

    more work on moduleiohardware

--- src/main/java/frc/robot/Subsystems/Drive/Module/ModuleIOHardware.java
+++ src/main/java/frc/robot/Subsystems/Drive/Module/ModuleIOHardware.java
@@ -1,12 +1,16 @@
-package main.java.frc.robot.Subsystems.Drive.Module;
+package frc.robot.Subsystems.Drive.Module;
 
 import java.util.Queue;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants;
 
 public class ModuleIOHardware implements ModuleIO{
     private final Rotation2d rotationOffset;
@@ -20,7 +24,7 @@ public class ModuleIOHardware implements ModuleIO{
     private final Queue<Double> drivePosQueue;
     private final Queue<Double> turnPosQueue;
 
-    private double absoluteEncoderOffset;
+    private Rotation2d absoluteEncoderOffset;
     private final int driveMotorID;
     private final int turnMotorID;
 
@@ -44,7 +48,7 @@ public class ModuleIOHardware implements ModuleIO{
                 case 1 -> Constants.DriveConstants.frontRightDriveCanId;
                 case 2 -> Constants.DriveConstants.backLeftDriveCanId;
                 case 3 -> Constants.DriveConstants.backRightDriveCanId;
-                default -> -999999
+                default -> -999999;
             };
         
         turnMotorID =
@@ -56,10 +60,55 @@ public class ModuleIOHardware implements ModuleIO{
                 default -> -999999;
             };
 
+        absoluteEncoder = 
+            new CANcoder(
+                switch(moduleID){
+                    case 0 -> Constants.DriveConstants.frontLeftTurnEncoder;
+                    case 1 -> Constants.DriveConstants.frontRightTurnEncoder;
+                    case 2 -> Constants.DriveConstants.backLeftTurnEncoder;
+                    case 3 -> Constants.DriveConstants.backRightTurnEncoder;
+                    default -> -999999;
+                }
+            );
+
+        absoluteEncoder.clearStickyFault_BadMagnet();
+        
+        TalonFXConfiguration driveConfiguration = new TalonFXConfiguration();
+        driveConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
+        driveConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
+        driveConfiguration.CurrentLimits.StatorCurrentLimit = 80;
+        driveConfiguration.CurrentLimits.SupplyCurrentLimit = 80;
+        
+
+        //can't be bothered to figure out if reverse if "InvertedValue.CounterClockwise_Positive"
+        // driveConfiguration.MotorOutput.Inverted =
+        //         switch(moduleID){
+        //             case 0 -> (InvertedValue) Constants.DriveConstants.frontLeftDriveInverted;
+        //             case 1 -> Constants.DriveConstants.frontRightDriveInverted;
+
+        //         };
+
+
         
 
         
+        //blah, blah, blah: other stuff I'll do later
+        
         
 
+    }
+
+    @Override
+    public void setDriveOpenLoop(double power){
+        //there's probably a better way to do this...
+        VoltageOut voltageRequest = new VoltageOut (power);
+        driveMotor.setControl(voltageRequest.withOutput(power));
+    }
+
+    @Override
+    public void setTurnOpenLoop(double power){
+        //there's probably a better way to do this...
+        VoltageOut voltageRequest = new VoltageOut (power);
+        turnMotor.setControl(voltageRequest.withOutput(power));
     }
 }
